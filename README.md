09a4be442190bce5c75c66560441cefb44c7211c
Roman <zipo.ckorop@gmail.com>
2020-04-20 12:04:35 +0300

    deploy instructions and init script

--- README.md
+++ README.md
@@ -1,2 +1,115 @@
 ## Docker Local environment for PHP projects
 
+1. Create directories structure:
+    ```
+    /home/${USER}/webserver
+    /home/${USER}/webserver/data
+    /home/${USER}/webserver/data/env_files
+    /home/${USER}/webserver/data/nginx
+    /home/${USER}/webserver/data/nginx/config
+    /home/${USER}/webserver/data/nginx/log
+    /home/${USER}/webserver/data/mysql/data
+    /home/${USER}/webserver/data/mysql/config
+    /home/${USER}/webserver/data/composer-cache
+    /home/${USER}/webserver/www
+    ```
+   
+2. Create/pull all images:
+    ```shell script
+    docker build ./PHP/7.3 -t php-fpm:7.3
+    docker pull nginx:1.17
+    docker pull mysql:8.0
+    docker pull jwilder/nginx-proxy
+    ```
+
+3. Global env settings:
+
+    3.1. MySQL `/home/${USER}/webserver/data/env_files/mysql.env`:
+    ```
+    MYSQL_ROOT_PASSWORD=rootpassword
+    ```
+   
+    3.2. nginx `/home/${USER}/webserver/data/env_files/nginx.env`:
+    ```
+    VIRTUAL_HOST=app-1-hostname,app-2-hostname, ...
+    ```
+
+4. Config files:
+
+    4.1. Copy `./Nginx/http.conf` to `/home/${USER}/webserver/data/nginx/config/http.conf` and
+    `./Nginx/default.conf` to `/home/${USER}/webserver/data/nginx/your-site.com.conf` 
+    replacing `${HOST-NAME}` with real host of new app.
+    
+    4.2. Copy `./MySQL/my.cnf` to `/home/${USER}/webserver/data/mysql/config/my.cnf`.
+   
+5. Create containers:
+    ```shell script
+    cd /home/${USER}/webserver
+    
+    # nginx-proxy
+    docker run -d \
+        -p 8080:80 \
+        -v /var/run/docker.sock:/tmp/docker.sock:ro \
+        --name webserver-nginx-proxy \
+        --net webserver-network \
+        jwilder/nginx-proxy 
+    
+    # MySql
+    docker run -d \
+        -v `pwd`/data/mysql/data:/var/lib/mysql:delegated \
+        -v `pwd`/data/mysql/config:/etc/mysql/conf.d:ro \
+        --name webserver-mysql \
+        --net webserver-network \
+        --env-file `pwd`/data/env_files/mysql.env \
+        mysql:8.0 \
+        --default-authentication-plugin=mysql_native_password
+    
+    # PHP
+    docker run -d \
+        -v `pwd`/www:/var/www:cached \
+        -v `pwd`/data/composer-cache:/home/php-user/composer-cache:delegated \
+        --name webserver-php-7.3 \
+        --net webserver-network \
+        seobar/php-fpm:latest
+   
+    # Add "--env-file `pwd`/data/env_files/php-local.env \" if you need pass env vars
+    
+    # nginx
+    docker run -d \
+        -v `pwd`/www:/var/www:cached \
+        -v `pwd`/data/nginx/config:/etc/nginx/conf.d:ro \
+        -v `pwd`/data/nginx/log:/var/log/nginx:delegated \
+        --name webserver-nginx \
+        --net webserver-network \
+        --env-file `pwd`/data/env_files/nginx.env \
+        nginx:1.17
+    
+6. Create mysql database using root mysql user:
+    ```shell script
+    $ docker exec -it seobar-mysql mysql -uroot -p 
+    # then enter password which you specified in /home/${USER}/webserver/data/env_files/mysql.env
+    ```
+    ```mysql
+    # Create db
+    CREATE DATABASE db_name;
+    
+    # Create db user
+    CREATE USER 'username'@'%' IDENTIFIED BY 'user_password';
+    
+    # Grant all privileges to username
+    GRANT ALL PRIVILEGES ON db_name.* TO 'username'@'%';
+    
+    # Flush all privileges
+    FLUSH PRIVILEGES;
+    
+    # check user privileges
+    SHOW GRANTS FOR 'username'@'%';
+    
+    # The output should be:
+    # +------------------------------------------------------+
+    # | Grants for username@%                                |
+    # +------------------------------------------------------+
+    # | GRANT USAGE ON *.* TO `username`@`%`                 |
+    # | GRANT ALL PRIVILEGES ON `db_name`.* TO `username`@`%`|
+    # +------------------------------------------------------+
+    ```
---
+++ init.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+CLEAR_COLORS="\\e[0m"
+COLOR_BG_YELLOW="\e[30;43m"
+COLOR_BG_RED="\e[30;41m"
+COLOR_BG_GREEN="\e[30;42m"
+COLOR_BG_BLUE="\e[30;44m"
+
+function warning() {
+  echo -e "$COLOR_BG_YELLOW Warning: $1 $CLEAR_COLORS"
+}
+function error() {
+  echo -e "$COLOR_BG_RED Error: $1 $CLEAR_COLORS"
+}
+function success() {
+  echo -e "$COLOR_BG_GREEN OK: $1 $CLEAR_COLORS"
+}
+function info() {
+  echo -e "$COLOR_BG_BLUE $1 $CLEAR_COLORS"
+}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --path=*)
+      WEBSERVER_PATH="${1#*=}"
+      ;;
+    *)
+      error "Invalid argument $1"
+      exit 1
+  esac
+  shift
+done
+
+if [ "$WEBSERVER_PATH" == "" ] ; then
+  error "\"--path\" argument is not provided."
+  exit 1
+fi
+
+if [ -d "$WEBSERVER_PATH" ] && [ "$(ls -A $WEBSERVER_PATH)" ]; then
+  error "Directory '$WEBSERVER_PATH' already exists and it's not empty."
+  exit 1
+fi
+
+# Set MySQL Root password
+echo 'Enter MySql root password:'
+read -r MYSQL_ROOT_PASSWORD
+
+# Set MySQL User
+echo 'Enter MySql new username:'
+read -r MYSQL_USERNAME
+echo "Enter password for \"$MYSQL_USERNAME\":"
+read -r MYSQL_USERNAME_PASSWORD
+
+
+# Set Nginx virtual hosts
+WEBSERVER_HOSTS=''
+while true; do
+  if [ "$WEBSERVER_HOSTS" == "" ]; then
+    echo "Enter hostname:"
+  else
+    echo "Enter one more hostname or enter \"N\":"
+  fi
+
+  read -r NEW_HOST
+
+  if [ "$WEBSERVER_HOSTS" != "" ] && [ "$NEW_HOST" == "N" ]; then
+    break
+  fi
+  if [ "$WEBSERVER_HOSTS" == "" ]; then
+    WEBSERVER_HOSTS="$NEW_HOST"
+  else
+    WEBSERVER_HOSTS="$WEBSERVER_HOSTS,$NEW_HOST"
+  fi
+done
+
+# Create directory structure
+mkdir -p "$WEBSERVER_PATH/data/env_files"
+mkdir -p "$WEBSERVER_PATH/data/nginx/config"
+mkdir -p "$WEBSERVER_PATH/data/nginx/log"
+mkdir -p "$WEBSERVER_PATH/data/mysql/data"
+mkdir -p "$WEBSERVER_PATH/data/mysql/config"
+mkdir -p "$WEBSERVER_PATH/data/composer-cache"
+mkdir -p "$WEBSERVER_PATH/data/www"
+
+# Create env files
+echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" > "$WEBSERVER_PATH/data/env_files/mysql.env"
+echo "VIRTUAL_HOST=$WEBSERVER_HOSTS" > "$WEBSERVER_PATH/data/env_files/nginx.env"
+
+# Config files
+cp ./MySQL/my.cnf "$WEBSERVER_PATH/data/mysql/config/my.cnf"
+cp ./Nginx/basic-config.nginx "$WEBSERVER_PATH/data/nginx/config/basic-config.nginx"
+
+IFS=',' read -r -a HOSTS_ARRAY <<< "$WEBSERVER_HOSTS"
+for HOST in "${HOSTS_ARRAY[@]}"
+do
+  sed "s/your-site.com/$HOST/g" ./Nginx/default.conf > "$WEBSERVER_PATH/data/nginx/config/$HOST.conf"
+  mkdir -p "$WEBSERVER_PATH/www/$HOST"
+  printf "<?php\nphpinfo();" > "$WEBSERVER_PATH/www/$HOST/index.php"
+done
+
+# Build/pull docker images
+docker build ./PHP/7.3 -t php-fpm:7.3
+docker pull nginx:1.17
+docker pull mysql:8.0
+docker pull jwilder/nginx-proxy
+
+# Remove old containers if exists
+docker rm -f webserver-nginx-proxy webserver-nginx webserver-php-7.3 webserver-mysql
+
+# Create network
+docker network create webserver-network
+
+# Create containers
+docker run -d \
+    -p 8080:80 \
+    -v /var/run/docker.sock:/tmp/docker.sock:ro \
+    --name webserver-nginx-proxy \
+    --net webserver-network \
+    jwilder/nginx-proxy
+
+docker run -d \
+    -v "$WEBSERVER_PATH/data/mysql/data:/var/lib/mysql:delegated" \
+    -v "$WEBSERVER_PATH/data/mysql/config:/etc/mysql/conf.d:ro" \
+    --name webserver-mysql \
+    --net webserver-network \
+    --env-file "$WEBSERVER_PATH/data/env_files/mysql.env" \
+    mysql:8.0 \
+    --default-authentication-plugin=mysql_native_password
+
+docker run -d \
+    -v "$WEBSERVER_PATH/www:/var/www:cached" \
+    -v "$WEBSERVER_PATH/data/composer-cache:/home/php-user/composer-cache:delegated" \
+    --name webserver-php-7.3 \
+    --net webserver-network \
+    seobar/php-fpm:latest
+
+docker run -d \
+    -v "$WEBSERVER_PATH/www:/var/www:cached" \
+    -v "$WEBSERVER_PATH/data/nginx/config:/etc/nginx/conf.d:ro" \
+    -v "$WEBSERVER_PATH/data/nginx/log:/var/log/nginx:delegated" \
+    --name webserver-nginx \
+    --net webserver-network \
+    --env-file "$WEBSERVER_PATH/data/env_files/nginx.env" \
+    nginx:1.17
+
+while ! docker exec -t webserver-mysql mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "select 1"; do
+    echo "Wait for MySQL server 5 seconds..."
+    sleep 5
+done
+
+# Create MySql user
+docker exec -t webserver-mysql mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_USERNAME'@'%' IDENTIFIED BY '$MYSQL_USERNAME_PASSWORD';"
+
+# Create MySQL databases for each host
+for HOST in "${HOSTS_ARRAY[@]}"
+do
+  DATABASE="${HOST//[^a-z0-9]/_}"
+  docker exec -t webserver-mysql mysql -uroot -p"$MYSQL_ROOT_PASSWORD" \
+      -e "CREATE DATABASE $DATABASE;GRANT ALL PRIVILEGES ON $DATABASE.* TO '$MYSQL_USERNAME'@'%';"
+
+  info "Database \"$DATABASE\" was created."
+done
+docker exec -t webserver-mysql mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+
+success 'Done'
